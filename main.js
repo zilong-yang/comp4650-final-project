cc1de0b1bc7634357dc8b3834a913099221328d7
zilong-yang <yziLONG1325>
2020-04-01 23:58:38 -0400

    Updated some animations and added skeleton JS.

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    // alert("hello");
+
+    let create = document.getElementById("create-button");
+    let join = document.getElementById("join-button");
+
+    create.onclick = function () {
+        alert("create clicked");
+    };
+
+    join.onclick = function () {
+        alert("join clicked");
+    };
+};
